dfca5bc36dd28fff6d9f7716d38c13f7d288bd46
Nikita Volkov <volchonik634@gmail.com>
2024-07-14 08:53:04 +0300

    Create Python project

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import add
+
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
